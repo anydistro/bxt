ff08ba74769d4c3cba8bb3c4f14e3b0a61e3cd30
Daniil Lyudvig <thricht3r@yandex.ru>
2024-07-19 14:33:42 +0000

    feat(daemon): add endpoints to openapi.yml.in

--- daemon/core/application/services/AuthService.cpp
+++ daemon/core/application/services/AuthService.cpp
@@ -1,6 +1,7 @@
 /* === This file is part of bxt ===
  *
  *   SPDX-FileCopyrightText: 2022 Artem Grinev <agrinev@manjaro.org>
+ *   SPDX-FileCopyrightText: 2024 Daniil Lyudvig <thricht3r@yandex.ru>
  *   SPDX-License-Identifier: AGPL-3.0-or-later
  *
  */
